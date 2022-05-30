886f6f5869a404eb3491f5dcbcfb4680500a4211
m.b789 <m.burk@cse-it.net>
2022-05-30 19:25:36 +0200

    added AngularProject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,17 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent {
   title = 'train-app';
+  
+
+  onSave(event?: MouseEvent) {
+    const evtMsg = event ? ' Event target is ' + (event.target as HTMLElement).textContent : '';
+    alert('Saved.' + evtMsg);
+    if (event) { event.stopPropagation(); }
+  }
 }
+
+alert("Test");
